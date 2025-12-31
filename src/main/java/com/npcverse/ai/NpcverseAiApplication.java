ecede1dfbbe061ca02699241d61228da08ea1581
Ranindu Amarasinghe <raninduamarasinghe@gmail.com>
2025-12-31 10:43:58 +0530

    first commit

---
+++ src/main/java/com/npcverse/ai/NpcverseAiApplication.java
@@ -0,0 +1,13 @@
+package com.npcverse.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NpcverseAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NpcverseAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/npcverse/ai/NpcverseAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.npcverse.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NpcverseAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
